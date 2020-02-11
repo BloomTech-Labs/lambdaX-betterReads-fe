ce50267b6f3a8feb5621143c1de73f3020f21b46
Philip Johnson <phil7j@gmail.com>
2020-02-11 14:19:17 -0800

    Basic boilerplate setup

---
+++ src/Components/Homepage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const HomePage = props => {
+
+    return(
+
+        <Home>
+            <h1>Home</h1>
+        </Home>
+
+    )
+
+}
+const Home = styled.div`
+    background: purple;
+    height: 100%;
+    border: 2px dashed yellow;
+`;
+
+export default HomePage;
