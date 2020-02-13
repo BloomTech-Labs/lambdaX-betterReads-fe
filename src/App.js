48bdb9049eb00861f651d0bcf0254f9af9e739d7
Derek S <dschwan21@me.com>
2020-02-12 16:27:39 -0800

    receiving token from okta
    Co-authored-by: Phil <phil7j@protonmail.com>
    Co-authored-by: Max <maxgunter99@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import React from 'react';
-import './App.css';
-import { Route , NavLink } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+// Okta
+import { Security, ImplicitCallback } from "@okta/okta-react";
 
 // Componentss
-import HomePage from './Components/Homepage';
+import HomePage from "./Components/Homepage";
+import WithAuth from "./Components/WithAuth";
+
+// Okta config
+const config = {
+  issuer: "https://dev-640497.okta.com/oauth2/default",
+  redirectUri: window.location.origin + "/implicit/callback",
+  clientId: "0oa274tam6nSE47LW4x6",
+  pkce: true
+};
 
 function App() {
-
   return (
-
     <div className="App">
+      <Router>
+        <Security {...config}>
+          <Route path="/" exact={true} component={WithAuth} />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+        </Security>
+      </Router>
 
-      <div className = 'Routes'>
+      {/* <NavBar /> */}
+      {/* <div className="Routes">
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink exact to="/">
+          Profile
+        </NavLink>
+        <NavLink exact to="/">
+          Bookshelves{" "}
+        </NavLink>
+        <NavLink exact to="/">
+          Discover
+        </NavLink>
 
-        <NavLink exact to = '/'>Home</NavLink>
-        <NavLink exact to = '/'>Profile</NavLink>
-        <NavLink exact to = '/'>Bookshelves </NavLink>
-        <NavLink exact to = '/'>Discover</NavLink>
-
-      </div>
-
-      {/* Routes with attatched components */}
-      <Route exact path = '/' component = { HomePage } />
-
+        <WithAuth />
+      </div> */}
     </div>
-
   );
-
 }
 
 export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Route, NavLink } from "react-router-dom";
+import WithAuth from "./WithAuth";
+// Components
+import HomePage from "./Homepage";
+
+function NavBar() {
+  const [isAuthenticated, setAuth] = useState(false);
+
+  return (
+    <div className="App">
+      <div className="Routes">
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink exact to="/">
+          Profile
+        </NavLink>
+        <NavLink exact to="/">
+          Bookshelves{" "}
+        </NavLink>
+        <NavLink exact to="/">
+          Discover
+        </NavLink>
+
+        <WithAuth />
+
+        {/* <button>Login</button>
+        <button>Logout</button> */}
+      </div>
+
+      {/* Routes with attatched components */}
+      <Route exact path="/" component={HomePage} />
+    </div>
+  );
+}
+
+export default NavBar;
