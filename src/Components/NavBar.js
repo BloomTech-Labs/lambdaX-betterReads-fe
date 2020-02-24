e176b808baf2a08509c3144b16f26960d66783f0
zacharyarney <zachary.arney@gmail.com>
2020-02-23 22:59:00 -0800

    formatting

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,49 +1,56 @@
-import React, { useState } from "react";
-import { Route, NavLink } from "react-router-dom";
-import WithAuth from "./WithAuth";
-import BetterReadsLogo from '../Images/BetterReadsLogo.jpeg'
-import styled from "styled-components";
+import React, { useState } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import WithAuth from './WithAuth';
+import LoginButton from './LoginButton';
+import BetterReadsLogo from '../Images/BetterReadsLogo.jpeg';
+import styled from 'styled-components';
 
 function NavBar() {
+  const [isAuthenticated, setAuth] = useState(false);
+  const [search, setSearch] = useState('');
 
-  const [ isAuthenticated , setAuth ] = useState(false);
-  const [ search , setSearch ] = useState( '' );
-
-  function handleSearch( e ) {
-
-    setSearch( e.target.value );
-
-  };
+  function handleSearch(e) {
+    setSearch(e.target.value);
+  }
 
   return (
-
     <Routes>
+      <Logo src={BetterReadsLogo} />
 
-      <Logo src = { BetterReadsLogo }/>
+      <NavLink exact to="/">
+        {' '}
+        Bookshelves{' '}
+      </NavLink>
 
-      <NavLink exact to="/"> Bookshelves </NavLink>
+      <NavLink exact to="/">
+        {' '}
+        Discover{' '}
+      </NavLink>
 
-      <NavLink exact to="/"> Discover </NavLink>
+      <NavLink exact to="/Profile">
+        {' '}
+        Profile{' '}
+      </NavLink>
 
-      <NavLink exact to="/Profile"> Profile </NavLink>
-
-      <WithAuth/>
+      {/* <WithAuth /> */}
+      <LoginButton />
 
       <input
-        value = { search }
-        placeholder = 'Search Reads'
-        onChange = { handleSearch }
+        value={search}
+        placeholder="Search Reads"
+        onChange={handleSearch}
       />
 
-      <ProfileImage src = {'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS73zR9Totq6-MSUAshZ7E_RM4gbjIFymJh4oCmlPbxkT6Nnp38'} />
-
+      <ProfileImage
+        src={
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS73zR9Totq6-MSUAshZ7E_RM4gbjIFymJh4oCmlPbxkT6Nnp38'
+        }
+      />
     </Routes>
-
   );
 }
 
 const Routes = styled.div`
-
   display: flex;
   justify-content: space-evenly;
   align-items: center;
@@ -51,58 +58,49 @@ const Routes = styled.div`
   max-width: 100%;
   margin: 10px 0px 10px 0px;
 
-  @media (max-width: 560px) { 
+  @media (max-width: 560px) {
     margin: 0 auto;
   }
 
   a {
-
     padding: 10px;
     color: white;
-    text-decoration-color: rgb(22,35,43);
+    text-decoration-color: rgb(22, 35, 43);
 
     .active {
       color: red;
     }
-
   }
 
   input {
-
     width: 150px;
     height: 25px;
-    dusplay: flex;
+    display: flex;
     text-align: center;
     border-radius: 5px;
     border: none;
     outline: none;
-
   }
 
-  @media (max-width: 520px) { 
+  @media (max-width: 520px) {
     margin: 0 auto;
     margin-top: -50px;
   }
-
 `;
 
-const ProfileImage = styled.img `
-
+const ProfileImage = styled.img`
   height: 50px;
   width: 50px;
   border-radius: 100%;
   margin-left: 20px;
   margin-right: 20px;
   border: 1px solid white;
-
 `;
 
 const Logo = styled.img`
-
   height: 50px;
   width: 50px;
-  // margin-left: 20px;
-
+  /* margin-left: 20px; */
 `;
 
 export default NavBar;
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 // THIS SHOULD BE REPLACED WITH THE USERS INFO FROM STATE ONCE WE GET THE GOOGLE INFO / LOGIN WORKING
 // Simulates the info we will have once collecting data from our database:
 
-// note "Batteries Included": 
+// note "Batteries Included":
 //      I followed the database diagram to be accurate without using the database: https://dbdiagram.io/d/5e28d3619e76504e0ef08ed5
 //      Styled using the wireframe: https://www.figma.com/file/LN9bPENjN2JU8NNRWjfsE5/Better-Reads?node-id=0%3A1
-
 
 // PROPOSAL: Could we store the isbn number of the book in the "bookshelf_book" table to reduce a layer of abstaction?
 // also making the books an array with all of them to get the length for the profile bookshelf page
@@ -84,431 +83,349 @@ import styled from "styled-components";
 // --------------
 
 const user = {
+  id: 1, // Primary Key
+  username: 'AWatts',
+  email: 'AlanWatts@gmail.com',
+  password: 'PhilosophyIsCool100',
+  location: 'Austin',
+  bio: '',
+  image:
+    'https://i0.wp.com/www.brainpickings.org/wp-content/uploads/2014/01/alanwatts.jpg?w=680&ssl=1',
+  banner_image:
+    'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 
-    id: 1, // Primary Key
-    username: 'AWatts',
-    email: 'AlanWatts@gmail.com',
-    password: 'PhilosophyIsCool100',
-    location: 'Austin',
-    bio: '',
-    image: 'https://i0.wp.com/www.brainpickings.org/wp-content/uploads/2014/01/alanwatts.jpg?w=680&ssl=1',
-    banner_image: 'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  // Extras we could add to the database?
+  firstName: 'Alan',
+  lastName: 'Watts',
+  following: 1,
+  followers: 279,
 
-    // Extras we could add to the database?
-    firstName: 'Alan',
-    lastName: 'Watts',
-    following: 1,
-    followers: 279,
+  // All the users bokshelves they have created ⬇︎
+  user_bookshelves: [
+    // ORIGINAL
+    // {
 
-    // All the users bokshelves they have created ⬇︎
-    user_bookshelves: [
+    //     id: 1, // Primary Key
+    //     shelf_name: 'Plilosophy',
+    //     user_id: 1,
+    //     user_type: 'Creator', // Idea💡: we could use the values Creator / Author or Viewer / Reader ( for editing privileges )
 
-        // ORIGINAL
-        // {
+    //     // Books Inside the bookshelf ⬇︎
+    //     bookshelf_book: {
 
-        //     id: 1, // Primary Key
-        //     shelf_name: 'Plilosophy',
-        //     user_id: 1,
-        //     user_type: 'Creator', // Idea💡: we could use the values Creator / Author or Viewer / Reader ( for editing privileges )
+    //         id: 1, // Primary Key
+    //         book_id: 1,
+    //         bookshelf_id: 1,
 
-        //     // Books Inside the bookshelf ⬇︎
-        //     bookshelf_book: {
+    //         book: {
 
-        //         id: 1, // Primary Key
-        //         book_id: 1,
-        //         bookshelf_id: 1,
+    //             id: 1, // Primary Key
+    //             isbn: 1253
 
-        //         book: {
+    //         }
 
-        //             id: 1, // Primary Key
-        //             isbn: 1253
+    //     },
 
-        //         }
+    //     bookshelf_book: {
 
-        //     },
+    //         id: 2, // Primary Key
+    //         book_id: 2,
+    //         bookshelf_id: 1,
 
-        //     bookshelf_book: {
+    //         book: {
 
-        //         id: 2, // Primary Key
-        //         book_id: 2,
-        //         bookshelf_id: 1,
+    //             id: 2, // Primary Key
+    //             isbn: 8723
 
-        //         book: {
+    //         }
 
-        //             id: 2, // Primary Key
-        //             isbn: 8723
+    //     }
+    // },
 
-        //         }
+    // {
 
-        //     }
-        // },
+    //     id: 2, // Primary Key
+    //     shelf_name: 'Fun',
+    //     user_id: 1,
+    //     user_type: 'Creator', // Idea💡: we could use the values Creator / Author or Viewer / Reader ( for editing privileges )
 
-        // {
-            
-        //     id: 2, // Primary Key
-        //     shelf_name: 'Fun',
-        //     user_id: 1,
-        //     user_type: 'Creator', // Idea💡: we could use the values Creator / Author or Viewer / Reader ( for editing privileges )
+    //     bookshelf_book: {
 
-        //     bookshelf_book: {
+    //         id: 1, // Primary Key
+    //         book_id: 1,
+    //         bookshelf_id: 2,
 
-        //         id: 1, // Primary Key
-        //         book_id: 1,
-        //         bookshelf_id: 2,
+    //         book: {
 
-        //         book: {
+    //             id: 1, // Primary Key
+    //             isbn: 6301
 
-        //             id: 1, // Primary Key
-        //             isbn: 6301
+    //         }
 
-        //         }
+    //     },
 
-        //     },
+    //     bookshelf_book: {
 
-        //     bookshelf_book: {
+    //         id: 2, // Primary Key
+    //         book_id: 2,
+    //         bookshelf_id: 2,
 
-        //         id: 2, // Primary Key
-        //         book_id: 2,
-        //         bookshelf_id: 2,
+    //         book: {
 
-        //         book: {
+    //             id: 2, // Primary Key
+    //             isbn: 1287
 
-        //             id: 2, // Primary Key
-        //             isbn: 1287
+    //         }
 
-        //         }
+    //     }
+    // }
 
-        //     }
-        // }
+    // TESTING
 
-        // TESTING
+    {
+      id: 1, // Primary Key
+      shelf_name: 'Plilosophy',
+      user_id: 1,
+      user_type: 'Creator',
 
+      bookshelf_books: [
         {
-            id: 1, // Primary Key
-            shelf_name: 'Plilosophy',
-            user_id: 1,
-            user_type: 'Creator',
-
-            bookshelf_books: [
-
-                {
-                    id: 1,
-                    book_id: 1,
-                    bookshelf_id: 1,
-                    isbn: 1253
-                }
-            ],
+          id: 1,
+          book_id: 1,
+          bookshelf_id: 1,
+          isbn: 1253,
         },
+      ],
+    },
 
+    {
+      id: 2, // Primary Key
+      shelf_name: 'Fun',
+      user_id: 1,
+      user_type: 'Creator',
+
+      bookshelf_books: [
         {
-            id: 2, // Primary Key
-            shelf_name: 'Fun',
-            user_id: 1,
-            user_type: 'Creator',
+          id: 1,
+          book_id: 1,
+          bookshelf_id: 2,
+          isbn: 1045,
+        },
+        {
+          id: 2,
+          book_id: 2,
+          bookshelf_id: 2,
+          isbn: 7394,
+        },
+      ],
+    },
+  ],
+};
 
-            bookshelf_books: [
+const Profile = (props) => {
+  return (
+    <ProfileContainer>
+      <BackgroundImageContainer>
+        <BackgroundImage src={user.banner_image} />
+      </BackgroundImageContainer>
 
-                {
-                    id: 1,
-                    book_id: 1,
-                    bookshelf_id: 2,
-                    isbn: 1045
-                },
-                {
-                    id: 2,
-                    book_id: 2,
-                    bookshelf_id: 2,
-                    isbn: 7394
-                }
-            ],
-        }
+      <div className="Section Profile">
+        <img className="ProfilePicture" src={user.image} />
+      </div>
 
+      <UserInfoContainer>
+        <div className="Section User">
+          <h1 className="Name">
+            {user.firstName} {user.lastName}
+          </h1>
+          <p className="Location">{user.location}</p>
+        </div>
 
-    
-    ]
+        <div className="Section Social">
+          <div>
+            <h1>{user.followers}</h1>
+            <p>Followers</p>
+          </div>
 
-}
+          <div>
+            <h1>{user.following}</h1>
+            <p>Following</p>
+          </div>
+        </div>
 
-const Profile = props => {
+        <div className="Section Buttons">
+          <button className="Follow">Follow</button>
+          <button className="Message">Message</button>
+        </div>
+      </UserInfoContainer>
 
-    return(
-
-        <ProfileContainer>
-
-            <BackgroundImageContainer>
-
-                <BackgroundImage src = {user.banner_image}/>
-
-            </BackgroundImageContainer>
-
-            <div className = 'Section Profile'>
-
-                <img className = 'ProfilePicture' src = { user.image }/>
-
+      <BookshelvesAndActivityContainer>
+        <Bookshelves>
+          <h2>{user.firstName}'s Bookshelves</h2>
+          {user.user_bookshelves.map((book) => (
+            <div className="SingleBookshelf">
+              <p>{book.shelf_name}</p>
+              <p>{book.bookshelf_books.length}</p>
             </div>
+          ))}
+        </Bookshelves>
 
-            <UserInfoContainer>
-
-
-                <div className = 'Section User'>
-
-                    <h1 className = 'Name'>{ user.firstName } { user.lastName }</h1>
-                    <p className = 'Location'>{ user.location }</p>
-
-                </div>
-
-                <div className = 'Section Social'>
-
-                    <div>
-
-                        <h1>{ user.followers }</h1>
-                        <p>Followers</p>
-
-                    </div>
-
-                    <div>
-
-                        <h1>{ user.following }</h1>
-                        <p>Following</p>
-
-                    </div>
-
-                </div>
-
-                <div className = 'Section Buttons'>
-
-                    <button className = 'Follow'>Follow</button>
-                    <button className = 'Message'>Message</button>
-
-                </div>
-
-            </UserInfoContainer>
-
-
-            <BookshelvesAndActivityContainer>
-            
-                <Bookshelves>
-
-                    <h2>{ user.firstName }'s Bookshelves</h2>
-                    { user.user_bookshelves.map( book =>
-
-                        <div className = 'SingleBookshelf'>
-                            <p>{ book.shelf_name }</p>
-                            <p>{ book.bookshelf_books.length }</p>
-                        </div>
-
-                    )}
-
-                </Bookshelves>
-
-                <Activity>
-
-                    <h1>Activity</h1>
-
-                </Activity>
-            
-            </BookshelvesAndActivityContainer>
-
-        </ProfileContainer>
-
-    )
-
+        <Activity>
+          <h1>Activity</h1>
+        </Activity>
+      </BookshelvesAndActivityContainer>
+    </ProfileContainer>
+  );
 };
 
 const ProfileContainer = styled.div`
+  // background: linear-gradient( rgb( 23 , 35 , 43 ) , rgb( 58 , 95 , 114 ) );
+  min-height: 90vh;
+  max-width: 1500px;
+  margin: 0 auto;
 
-    // background: linear-gradient( rgb( 23 , 35 , 43 ) , rgb( 58 , 95 , 114 ) );
-    min-height: 90vh;
-    max-width: 1500px;
-    margin: 0 auto;
+  h1 {
+    margin: 5px;
+  }
 
-    h1 {
+  p {
+    margin: 10px;
+    color: rgb(171, 171, 171);
+  }
 
-        margin: 5px;
+  .Section.Profile {
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    overflow: hidden;
+    border-radius: 50%;
+    border: 1px solid white;
+    margin-top: -100px;
+    margin-left: 50px;
+  }
 
-    }
-
-    p {
-
-        margin: 10px;
-        color: rgb( 171, 171, 171 );
-
-    }
-
-    .Section.Profile {
-
-        position: absolute;
-        width: 200px;
-        height: 200px;
-        overflow: hidden;
-        border-radius: 50%;
-        border: 1px solid white;
-        margin-top: -100px;
-        margin-left: 50px;
-
-    }
-
-    .ProfilePicture {
-
-        width: 100%;
-        height: auto;
-
-    }
-
+  .ProfilePicture {
+    width: 100%;
+    height: auto;
+  }
 `;
 
 const BackgroundImageContainer = styled.div`
-
-    width: 100%;
-    height: 400px;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
+  width: 100%;
+  height: 400px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const BackgroundImage = styled.img`
-
-    width: auto;
-    height: auto;
-    min-height: 400px;
-
+  width: auto;
+  height: auto;
+  min-height: 400px;
 `;
 
 const UserInfoContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  margin-left: 300px;
+  justify-content: space-between;
+  margin-bottom: 50px;
 
+  .Section {
+    color: white;
+  }
+
+  .Section.User {
+    width: 200px;
+  }
+
+  .Section.Social {
     display: flex;
-    margin: 0 auto;
-    margin-left: 300px;
-    justify-content: space-between;
-    margin-bottom: 50px;
+    width: 400px;
+    justify-content: space-evenly;
+  }
 
-    .Section {
+  .Buttons {
+    display: flex;
+    width: 400px;
+    justify-content: space-evenly;
+    align-items: center;
+    transition: 0.5s;
 
-        color: white;
-
+    button {
+      font-size: 25px;
+      height: 50px;
+      min-width: 150px;
+      border-radius: 10px;
+      border: 1px solid rgb(105, 121, 248);
     }
 
-    .Section.User {
-
-        width: 200px;
-
+    button.Follow {
+      background-color: rgb(105, 121, 248);
     }
 
-    .Section.Social {
-
-        display: flex;
-        width: 400px;
-        justify-content: space-evenly;
-
+    button.Message {
+      // border: 1px solid rgb(105,121,248);
     }
 
-    .Buttons {
-
-        display: flex;
-        width: 400px;
-        justify-content: space-evenly;
-        align-items: center;
-        transition: .5s;
-
-        button {
-
-            font-size: 25px;
-            height: 50px;
-            min-width: 150px;
-            border-radius: 10px;
-            border: 1px solid rgb(105,121,248);
-
-        }
-
-        button.Follow {
-
-            background-color: rgb(105,121,248);
-
-        }
-
-        button.Message {
-
-            // border: 1px solid rgb(105,121,248);
-
-        }
-
-        button:hover {
-
-            cursor: pointer;
-            border: 1px solid rgb(33,33,33);
-            transition: .5s;
-
-        }
+    button:hover {
+      cursor: pointer;
+      border: 1px solid rgb(33, 33, 33);
+      transition: 0.5s;
     }
-
+  }
 `;
 
-
 const BookshelvesAndActivityContainer = styled.div`
-
-    display: flex;
-    width: 90%;
-    margin: 0 auto;
-    justify-content: space-between;
-
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: space-between;
 `;
 
 const Bookshelves = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid white;
+  width: 300px;
+  color: white;
+  height: 100%;
 
+  h2 {
+    padding: 10px 0px 10px 0px;
+    margin: 0px;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+
+  .SingleBookshelf {
     display: flex;
-    flex-direction column;
-    align-items: center;
-    border: 1px solid white;
-    width: 300px;
-    color: white;
-    height: 100%;
+    justify-content: space-between;
+    width: 80%;
+    margin: 10px 0px 10px 0px;
+    border-bottom: 1px solid white;
 
-    h2 {
+    p {
+      color: white;
+    }
+  }
 
-        padding: 10px 0px 10px 0px;
-        margin: 0px;
-        width: 100%;
-        background-color: rgba(255, 255, 255, 0.2);
-
-    };
-
-    .SingleBookshelf {
-
-        display: flex;
-        justify-content: space-between;
-        width: 80%;
-        margin: 10px 0px 10px 0px;
-        border-bottom: 1px solid white;
-
-        p {
-
-            color: white;
-
-        }
-
-    };
-
-    .SingleBookshelf:hover {
-
-        background-color:rgba(255, 255, 255, 0.2);
-        transition: .5s;
-        cursor: pointer;
-
-    };
-
+  .SingleBookshelf:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+    transition: 0.5s;
+    cursor: pointer;
+  }
 `;
 
 const Activity = styled.div`
-
-    display: flex;
-    width: 66%;
-    justify-content: center;
-    border: 1px solid white;
-    min-height: 50vh;
-    margin-bottom: 50px;
-
+  display: flex;
+  width: 66%;
+  justify-content: center;
+  border: 1px solid white;
+  min-height: 50vh;
+  margin-bottom: 50px;
 `;
 
 export default Profile;
